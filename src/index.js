962e29612898751c9e6a43b989935e7a31e85c43
Steve Maibach <steve.maibach@heig-vd.ch>
2019-05-10 17:14:32 +0200

    implements filter all

--- src/index.js
+++ src/index.js
@@ -1,78 +1,126 @@
-import 'graph.js';
 import DATA from '../data/data2.js';
 import Chart from 'chart.js';
 import * as R from 'ramda';
 
 let ctx = $('#myChart');
 
-$("button").on('click', event => {
+let typeTab = [];
+let sort = 'imp';
+let x = [];
+let myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels:["2002", "2015", "2016", "2017"],
+    }
+});
 
+const getData = ({nom}) => R.find(d => d.nom === nom, DATA);
+const createCharteImp = ({imp, nom, couleur}) => ({
+  label: nom,
+  data: imp,
+  borderColor: [
+  couleur,
+  ],
+  backgroundColor: [
+'rgba(128, 128, 128, 0)',
+  ],
+  borderWidth: 4
+});
+
+const createCharteExp = ({exp, nom, couleur}) => ({
+  label: nom,
+  data: exp,
+  borderColor: [
+  couleur,
+  ],
+  backgroundColor: [
+'rgba(128, 128, 128, 0)',
+  ],
+  borderWidth: 4
+})
+
+$("#first > button").on('click', event => {
 
     let type = $(event.currentTarget).data();
 
-/*  let imp = DATA.find(d => d.nom === type.name)
-                  .imp;
-    let exp = DATA.find(d => d.nom === type.name)
-                  .exp;
-*/
-    let imp = R.find(d => d.nom === type.name, DATA).imp;
-    let exp = R.find(d => d.nom === type.name, DATA).exp;
-/*
-    let imp = DATA.find(R.propEq('nom', type.name)).imp;    
-    let exp = DATA.find(R.propEq('nom', type.name)).exp; 
-*/ 
-  
-
-    let color = type.color;
-
-    let attr1 = "Importation "+type.name;
-    let attr2 = "Exportation "+type.name;
-
     if ($(event.currentTarget).hasClass("selected")) {
-        $(event.currentTarget).removeClass("selected");   
-        myChart.destroy();
-        myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-              labels:["2002", "2015", "2016", "2017"],
-            }
+    $(event.currentTarget).removeClass("selected");
+    let pos = typeTab.indexOf(type.name);
+    typeTab.splice(pos, 1);
+        
+   } else {
+        $(event.currentTarget).addClass("selected");
+        typeTab.push({
+          nom : type.name,
+          couleur: type.color});
+   };
+
+  if (sort === 'exp') {
+    x = typeTab.map(fruit => ({
+    ...fruit,
+    ...getData(fruit)
+   }))
+   .map(createCharteExp);
+      };
+
+   if (sort === 'imp') {
+    x = typeTab.map(fruit => ({
+    ...fruit,
+    ...getData(fruit)
+   }))
+   .map(createCharteImp);
+      };
+
+   $("button").trigger('change');   
 
 });
 
-   } else {
-        $("button").removeClass("selected");
-        $(event.currentTarget).addClass("selected");
-        myChart.destroy(); 
-            myChart = new Chart(ctx, {
+$("li").on('mouseover', event => {
+  $(event.currentTarget).addClass("selected")
+});
+
+$("li").on('mouseleave', event => {
+  $(event.currentTarget).removeClass("selected")
+});
+
+$(".sort").on('click', event => {
+
+
+  $('.sort').removeClass("selected");
+  $(event.currentTarget).addClass("selected"); 
+
+  sort = $(event.currentTarget).data().sort; 
+
+  if (sort === 'exp') {
+    x = typeTab.map(fruit => ({
+    ...fruit,
+    ...getData(fruit)
+   }))
+   .map(createCharteExp);
+      };
+
+   if (sort === 'imp') {
+    x = typeTab.map(fruit => ({
+    ...fruit,
+    ...getData(fruit)
+   }))
+   .map(createCharteImp);
+      };    
+
+  $("button").trigger('change');
+  
+});
+
+$("button").on('change', event => {
+
+   myChart.destroy();
+  
+   myChart = new Chart(ctx, {
 
     type: 'line',
     data: {
       labels:["2002", "2015", "2016", "2017"],
-      datasets: [
-               {
-          label: attr2,
-          data: exp,
-          backgroundColor: [
-          'rgba(128, 128, 128, 0.5)',
-          ],
-          borderColor: [
-          'rgba(128, 128, 128, 0.5)',
-          ],
-
-        },
-        {
-          label: attr1,
-          data: imp,
-          backgroundColor: [
-          color
-          ],
-          borderColor: [
-          color
-          ],
-          borderWidth: 5
-        },
-
-      ]
+      datasets: x
     },
     options: {
         legend: {
@@ -93,28 +141,7 @@ $("button").on('click', event => {
     }
 
 });
-   };
 
 });
-
-   let myChart = new Chart(ctx, {
-
-    type: 'line',
-    data: {
-      labels:["2002", "2015", "2016", "2017"],
-    }
-
-});
-
-
-
-$("li").on('mouseover', event => {
-  $(event.currentTarget).addClass("selected")
-});
-
-$("li").on('mouseleave', event => {
-  $(event.currentTarget).removeClass("selected")
-});
-
 
 
